b78726073caafbb555883a00b1a78d1d674f1d03
Hakia <157662216+Hakia-mask@users.noreply.github.com>
2024-04-25 11:44:23 +0700

    Update calcu.js

--- calcu.js
+++ calcu.js
@@ -1,56 +1,80 @@
-
 var screen = document.querySelector('#screen');
- var btn = document.querySelectorAll('.btn');
- 
- for (item of btn) {
- item.addEventListener('click', (e) => {
-	btntext = e.target.innerText;	
-	
-	if (btntext == 'x') {
-	btntext = '*';
+var btn = document.querySelectorAll('.btn');
+document.addEventListener('keyup',(e)=>{
+	if(e.keyCode == "13"){
+		screen.value=eval(screen.value.replaceAll('%', '*0.01'))
 	}
-	if (btntext == '÷') {
-	btntext = '/';
-	}
-	screen.value += btntext;
-});
- }
- function sin() {
-	 screen.value = Math.sin(screen.value);
- }
- function cos() {
-	 screen.value = Math.cos(screen.value);
- }
- function tan() {
-	 screen.value = Math.tan(screen.value);
- }
- function pow() {
-	 screen.value = Math.pow(screen.value, 2);/*pangkat*/
- }
- function sqrt() {
-	 screen.value = Math.sqrt(screen.value, 2);/*alkar*/
- }
-function log() {
-	 screen.value = Math.log(screen.value);
- }
- function pi() {
-	 screen.value = 3.14159265359;
- }
-function e() {
-	 screen.value = 2.71828182846;
- }
+})
+for(item of btn) {
 
-function fact(){
-	var i, num, f;
-	f = 1
-	num = screen.value;
-	for (i = 1; i <=num; i++){
-		f = f * i;
+	item.addEventListener('click',(e)=>{
+	  btntext = e.target.innerText;
+	  
+	  if(btntext =='x'){
+
+		  btntext='*';
+
+	  }
+	  if(btntext=='÷'){
+
+		  btntext='/';		  
+	  }
+	  screen.value += btntext;
+});
 	}
-	i = i - 1;
+	function sin(){
+		screen.value = Math.sin(screen.value);
+	}
+	function cos(){
+	screen.value = Math.cos(screen.value);
+	}
+	function tan(){
+		screen.value = Math.tan(screen.value);
+	}
+	function pow(){
+	screen.value = Math.pow(screen.value, 2);
+	}
+	function sqrt(){
+		screen.value = Math.sqrt(screen.value, 2);
+	}
+	function log(){
+	screen.value = Math.log(screen.value);
+	}
+	function pi(){
+		screen.value = 3.14159265359;
+	}
+	function e(){
+		screen.value = 2.71828182845; /*euler*/
+	}
+	function fact(){
+	var i, num, f;
+		f = 1 
+	num = screen.value;          /*faktor*/
+		for(i = 1; i <= num; i++){
+			f = f * i;
+		}
+		i = i - 1;
+		screen.value = f;
+	}
+	function backspc(){
+
+		screen.value = screen.value.substr(0, screen.value.length - 1);
+
+	}
+
+
 	
-	screen.value=f;
-}
-function backspc() {
-	screen.value = screen.value.substr(0, screen.value.length - 1);
-}
+
+	/* e=exponen
+
+	   ac=AllClear
+
+	   ce=ClearEntry
+
+	   sin=sinus
+
+	   cos=cosinus
+
+	   tan=tangen
+
+	   X!=faktorial*/
